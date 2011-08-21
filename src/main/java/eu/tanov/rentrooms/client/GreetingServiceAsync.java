18d28e78952ebc77435517dee75290863eebdf71
Plamen Tanov <1.plamen@gmail.com>
2011-08-21 19:58:51 +0300

    First mavenized version copied from sample new project and expenses sample project

---
+++ src/main/java/eu/tanov/rentrooms/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package eu.tanov.rentrooms.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
