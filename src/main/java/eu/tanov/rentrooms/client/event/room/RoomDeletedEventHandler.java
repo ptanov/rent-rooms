9d51f8649e7ab56b03bb78fe832b3c9aca6b9c3b
Plamen Tanov <1.plamen@gmail.com>
2011-10-08 23:21:44 +0300

    [#2]: Event is not used Implement deletion of room, http://code.google.com/p/rent-rooms/issues/detail?id=2

--- src/main/java/eu/tanov/rentrooms/client/event/room/RoomDeletedEventHandler.java
+++
@@ -1,7 +0,0 @@
-package eu.tanov.rentrooms.client.event.room;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface RoomDeletedEventHandler extends EventHandler {
-  void onRoomDeleted(RoomDeletedEvent event);
-}
